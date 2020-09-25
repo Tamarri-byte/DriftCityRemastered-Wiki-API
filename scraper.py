b750b4f5d39b7faa54dabcd3e96e3531e04bb09c
Tamarri-byte <69334990+Tamarri-byte@users.noreply.github.com>
2020-09-25 13:19:15 -0400

    Setting up part objects

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 url = 'https://drift-city-remastered.fandom.com/wiki/Speed_Parts'
 
@@ -9,11 +8,32 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 part_table = soup.find('table', class_ = 'article-table')
 
+list = []
+
+class currPart:
+    def __init__(self,currPartName,currPartType,currPartLevel):
+        self.currPartName = currPartName
+        self.currPartType = currPartType
+        self.currPartLevel = currPartLevel
+
+
 for team in part_table.find_all('tbody'):
     rows = team.find_all('tr')
     for row in rows:
-        partNames = row.find_all('td')
-        #print(partNames)
-        for part in partNames:
-            partData = part.text
-            print(partData.strip())
+        Part = row.find_all('td')
+        index = 0
+        for partData in Part:
+            print(str(index))
+            if(index == 0):
+                curr = partData.text
+                print(curr.strip())
+
+
+            if(index == 5):
+                index = 0
+            else:
+                index += 1
+                
+            
+
+            
